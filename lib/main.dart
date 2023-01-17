375cf65853cb6132e97769f4d944010068e5729c
steadysamwise4 <davenportsam44@gmail.com>
2023-01-17 08:00:32 -0600

    added and wired up user inputs, split app into widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
+import '../widgets/user_transactions.dart';
 import 'package:flutter/material.dart';
-import './transaction.dart';
-import 'package:intl/intl.dart';
 
 void main() => runApp(const MyApp());
 
@@ -19,15 +18,8 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatelessWidget {
   MyHomePage({super.key});
 
-  final List<Transaction> transactions = [
-    Transaction(
-        id: 't1', title: 'New Shoes', amount: 69.99, date: DateTime.now()),
-    Transaction(
-        id: 't2',
-        title: 'Weekly Groceries',
-        amount: 16.53,
-        date: DateTime.now()),
-  ];
+  // String titleInput;
+  // String amountInput;
 
   @override
   Widget build(BuildContext context) {
@@ -36,8 +28,7 @@ class MyHomePage extends StatelessWidget {
           title: const Text('Flutter App'),
         ),
         body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          crossAxisAlignment: CrossAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
             Container(
               width: double.infinity,
@@ -47,44 +38,7 @@ class MyHomePage extends StatelessWidget {
                 child: Text('CHART!'),
               ),
             ),
-            Column(
-              children: transactions.map((tx) {
-                return Card(
-                  child: Row(children: <Widget>[
-                    Container(
-                      margin: const EdgeInsets.symmetric(
-                          vertical: 10, horizontal: 15),
-                      decoration: BoxDecoration(
-                          border: Border.all(
-                        color: Colors.purple,
-                        width: 2,
-                      )),
-                      padding: const EdgeInsets.all(10),
-                      child: Text('\$${tx.amount}',
-                          style: const TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
-                            color: Colors.purple,
-                          )),
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text(tx.title,
-                            style: const TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold,
-                            )),
-                        Text(DateFormat.yMMMd().format(tx.date),
-                            style: const TextStyle(
-                              color: Colors.grey,
-                            )),
-                      ],
-                    )
-                  ]),
-                );
-              }).toList(),
-            )
+            const UserTransactions(),
           ],
         ));
   }
