79b03e25d11e4b90b1b4135a6ff864cf3c21fba4
steadysamwise4 <davenportsam44@gmail.com>
2023-01-22 08:39:36 -0600

    added waiting image to conditionally render when there are no transactions

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -11,45 +11,62 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return SizedBox(
       height: 300,
-      child: ListView.builder(
-        itemBuilder: (ctx, index) {
-          return Card(
-            child: Row(children: <Widget>[
-              Container(
-                margin:
-                    const EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                decoration: BoxDecoration(
-                    border: Border.all(
-                  color: Theme.of(context).colorScheme.primary,
-                  width: 2,
-                )),
-                padding: const EdgeInsets.all(10),
-                child:
-                    Text('\$${transactions[index].amount.toStringAsFixed(2)}',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20,
-                          color: Theme.of(context).colorScheme.primary,
-                        )),
+      child: transactions.isEmpty
+          ? Column(children: <Widget>[
+              Text(
+                'No transactions added yet!',
+                style: Theme.of(context).textTheme.headline6,
               ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text(
-                    transactions[index].title,
-                    style: Theme.of(context).textTheme.headline6,
-                  ),
-                  Text(DateFormat.yMMMd().format(transactions[index].date),
-                      style: const TextStyle(
-                        color: Colors.grey,
+              const SizedBox(
+                height: 20,
+              ),
+              SizedBox(
+                  height: 200,
+                  child: Image.asset(
+                    'assets/images/waiting.png',
+                    fit: BoxFit.cover,
+                  )),
+            ])
+          : ListView.builder(
+              itemBuilder: (ctx, index) {
+                return Card(
+                  child: Row(children: <Widget>[
+                    Container(
+                      margin: const EdgeInsets.symmetric(
+                          vertical: 10, horizontal: 15),
+                      decoration: BoxDecoration(
+                          border: Border.all(
+                        color: Theme.of(context).colorScheme.primary,
+                        width: 2,
                       )),
-                ],
-              )
-            ]),
-          );
-        },
-        itemCount: transactions.length,
-      ),
+                      padding: const EdgeInsets.all(10),
+                      child: Text(
+                          '\$${transactions[index].amount.toStringAsFixed(2)}',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                            color: Theme.of(context).colorScheme.primary,
+                          )),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          transactions[index].title,
+                          style: Theme.of(context).textTheme.headline6,
+                        ),
+                        Text(
+                            DateFormat.yMMMd().format(transactions[index].date),
+                            style: const TextStyle(
+                              color: Colors.grey,
+                            )),
+                      ],
+                    )
+                  ]),
+                );
+              },
+              itemCount: transactions.length,
+            ),
     );
   }
 }
